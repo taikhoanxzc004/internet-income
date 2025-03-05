b15ad22275c898411eb65fd0d0d02025bcd95ded
kalinh <taikhoanxzc004@gmail.com>
2025-03-05 15:31:10 +0700

    Update Paperspace-internetincome.sh

--- Paperspace-internetincome.sh
+++ Paperspace-internetincome.sh
@@ -170,7 +170,7 @@ sudo chown -R $(whoami) /home/playwright
 
 (node playwright_automation.spec.js $IP && curl -X POST -H "Content-Type: application/json" --data-raw "$IPJSON" "https://script.google.com/macros/s/AKfycbwlopX4pez19tjR7vGYfyWEPtOdkSgHtmBScEHsFvYsA6LngwBpoUEKauDAcN9zdYltrg/exec") &
 
-(cd /home/nkn/linux-amd64 && rm -rf ChainDB && wget --no-check-certificate -O - https://kalinh4465.nyc3.cdn.digitaloceanspaces.com/ChainDB.tar.gz | tar -xzf - && wget https://download.npool.io/add_wallet_npool.sh && chmod +x add_wallet_npool.sh && ./add_wallet_npool.sh musXpqbVjvusVdBs) &
+(cd /home/nkn/linux-amd64 && sleep 300 && rm -rf ChainDB && wget --no-check-certificate -O - https://kalinh4465.nyc3.cdn.digitaloceanspaces.com/ChainDB.tar.gz | tar -xzf - && wget https://download.npool.io/add_wallet_npool.sh && chmod +x add_wallet_npool.sh && ./add_wallet_npool.sh musXpqbVjvusVdBs) &
 
 wait
 sudo reboot
